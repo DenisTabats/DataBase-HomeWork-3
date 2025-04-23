52c7985287d7eac64e12f748ea7e044e3537a530
DenisTabats <159521408+DenisTabats@users.noreply.github.com>
2025-04-23 19:22:49 +0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+$link=false;
+function OpenDB()
+{
+    global $link;
+    $link = mysqli_connect("localhost", "root", "", "turfirma");
+    mysqli_query($link, "SET NAMES UTF8");
+}
+function CloseDB()
+{
+    global $link;
+    mysqli_close($link);
+}
+function getInfo()
+{
+    global $link;
+    OpenDB();
+    $result=mysqli_query($link, "SELECT * FROM clients");
+    CloseDB();
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+function deleteInfo($id)
+{
+    global $link;
+    OpenDB();
+    $delete=mysqli_query($link,"DELETE FROM clients WHERE IDclient=$id");
+    CloseDB();
+    return $delete;
+}
+?>
